96dc04b2817110f9dfbb1e45f7ce003bbb3f3d87
jrosenlund <119529299+jrosenlund@users.noreply.github.com>
2024-02-22 23:05:50 -0700

    Final commit for mission 7!

--- Mission06_Rosenlund/Controllers/HomeController.cs
+++ Mission06_Rosenlund/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mission06_Rosenlund.Models;
 using System.Diagnostics;
 
@@ -6,7 +7,7 @@ namespace Mission06_Rosenlund.Controllers
 {
     public class HomeController : Controller
     {
-        private MovieContext _context;
+        private MovieContext _context; // Database "Instance"
         public HomeController(MovieContext temp) // Constructor
         {
             _context = temp;
@@ -30,24 +31,92 @@ namespace Mission06_Rosenlund.Controllers
         [HttpGet]
         public IActionResult AddMovie() // Form page
         {
-            return View();
+            ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+
+            return View(new Movie());
         }
 
         [HttpPost] // Form submission
         public IActionResult AddMovie(Movie movie)
         {
+            if (ModelState.IsValid)  // Check form data
+            {
             _context.Movies.Add(movie); // Add to the database
             _context.SaveChanges();
 
             return View("Confirmation");
+            }
+            else
+            {
+                ViewBag.Categories = _context.Categories
+                    .OrderBy(x => x.CategoryName)
+                    .ToList();
+
+                return View(movie);
+            }
+            
         }
 
         public IActionResult Collection()
         {
             var movies = _context.Movies
+                .Include("Category") // Join to display category name
                 .OrderBy(x => x.Title).ToList();
 
             return View(movies);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var toEdit = _context.Movies
+                .Single(x => x.MovieId == id);
+
+            ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+
+            return View("AddMovie", toEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid) // Check for valid data
+            {
+                _context.Movies.Update(movie);
+                _context.SaveChanges();
+
+                return RedirectToAction("Collection");
+            }
+            else
+            {
+                ViewBag.Categories = _context.Categories
+                    .OrderBy(x => x.CategoryName)
+                    .ToList();
+
+                return View("AddMovie", movie);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var toDelete = _context.Movies
+                .Single(x => x.MovieId == id);
+
+            return View("ConfirmDelete", toDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Movie movie)
+        {
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return RedirectToAction("Collection");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Mission06_Rosenlund/Models/Movie.cs
+++ Mission06_Rosenlund/Models/Movie.cs
@@ -9,15 +9,18 @@ namespace Mission06_Rosenlund.Models
         public int MovieId { get; set;  }
         [ForeignKey("CategoryId")]
         public int? CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
+        [Required(ErrorMessage = "You must enter a title.")] // Title required
         public string Title { get; set; }
+        [Required(ErrorMessage = "You must enter a year.")]
+        [Range(1888, 10000, ErrorMessage = "You must enter a year after 1888.")] // Only later than 1888
         public int Year { get; set; }
         public string? Director { get; set; }
         public string? Rating { get; set; }
         public bool Edited { get; set; }
         public string? LentTo { get; set; }
         public bool CopiedToPlex { get; set; }
-        [MaxLength(25)] // Limit notes field
+        [MaxLength(25,ErrorMessage = "Please keep notes less than 25 characters.")] // Limit notes field
         public string? Notes { get; set; }
 
     }
